9e8d8afde3bb322803392225d200efbc40bf2fa1
NEWPLAN <enwplan001@163.com>
2018-10-07 21:10:08 +0800

    self-register-factory

---
+++ Factory/self_resigter_factory/Message.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class Message
+{
+  public:
+	virtual ~Message() {}
+
+	virtual void foo()
+	{
+	}
+};
---
+++ Factory/self_resigter_factory/Message1.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "MessageFactory.hpp"
+#include "Message.hpp"
+
+class Message1 : public Message
+{
+  public:
+	Message1()
+	{
+		std::cout << "message1" << std::endl;
+	}
+
+	Message1(int a)
+	{
+		std::cout << "message1" << std::endl;
+	}
+
+	~Message1()
+	{
+	}
+
+	void foo() override
+	{
+		std::cout << "message1" << std::endl;
+	}
+};
+
+//REGISTER_MESSAGE(Message1, "message1", 2);
+REGISTER_MESSAGE(Message1, "message1");
